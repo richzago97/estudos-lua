ccce64b651efaf9eb60e0cd26f5c962b47ecdef9
richzago97 <rich.czago@gmail.com>
2023-10-15 11:29:49 -0300

    feat: Nova aula concluida de meta tabelas

---
+++ Techiesse Cursos/M2/metatables.lua
@@ -0,0 +1,24 @@
+local function ex1()
+    -- local a = {}
+    local meta = {}
+    local a = setmetatable({}, meta)
+    print(getmetatable(a) == meta)
+end
+
+-- ex1()
+
+local function ex2()
+    local meta =
+    {
+        __tostring = function (self)
+            return "Nome: " .. self.name
+        end
+    }
+    local a = setmetatable({name = "Minha tabela"}, meta)
+    print(a)
+    a.name = "Novo nome"
+    print(a)
+end
+
+ex2()
+
